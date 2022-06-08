15f66e9c5badd068d88394db35157a14dac8e406
Kotal <soumya-kanti.kotal@capgemini.com>
2022-06-08 22:21:14 +0530

    commiting latest

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -2,9 +2,14 @@ package com.employee.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +18,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.employee.dao.EmployeeDaoImpl;
 import com.employee.dto.EmployeeDetails;
 import com.employee.service.IEmployeeService;
 
 @RestController
 public class EmployeeController {
   
-  Logger logger = LoggerFactory.getLogger(EmployeeDaoImpl.class);
+  Logger logger = LoggerFactory.getLogger(EmployeeController.class);
   
   @Autowired
   IEmployeeService employeeService;
   
+  @Autowired
+  Environment env;
+  
   @GetMapping("/employee/{eid}")
-  public ResponseEntity<?> fetchEmployeeDetailsById(@PathVariable("eid") int employeeId) {
+  public ResponseEntity<?> fetchEmployeeDetailsById(@PathVariable("eid") @Min(101) @Max(999) int employeeId) {
 	  logger.info("Into EmployeeController:fetchEmployeeDetailsById");
 	  try {
 		  return new ResponseEntity<EmployeeDetails>(employeeService.fetchEmployeeDetails(employeeId) , HttpStatus.OK);
@@ -52,7 +59,7 @@ public class EmployeeController {
   }
   
   @PostMapping("/employee")
-  public ResponseEntity<?> insertEmployee(@RequestBody EmployeeDetails empDetails){
+  public ResponseEntity<?> insertEmployee(@RequestBody @Valid EmployeeDetails empDetails){
 	  logger.info("Into EmployeeController:insertEmployee");
 	  try {
 		  employeeService.insertEmployee(empDetails);
@@ -68,7 +75,7 @@ public class EmployeeController {
   @GetMapping("/healthCheck")
   public ResponseEntity<String> checkDbHealth(){
 	  if(employeeService.checkDBHealth()) {
-		  return new ResponseEntity<String>("H2 database is running fine" , HttpStatus.OK);
+		  return new ResponseEntity<String>("H2 database is running fine." + env.getProperty("cloud.config.status") , HttpStatus.OK);
 	  }
 	  else {
 		  return new ResponseEntity<String>("Exception while accessing the H2 database" , HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/com/employee/dto/EmployeeDetails.java
+++ src/main/java/com/employee/dto/EmployeeDetails.java
@@ -1,9 +1,29 @@
 package com.employee.dto;
 
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class EmployeeDetails {
+	
+	@NotNull
+	@Min(3)
 	private int id;
+	
+	@NotBlank(message = "Employee name cannot be blank")
+	@Size(min=3, max=50)
 	private String employeeName;
+	
+	@NotBlank(message = "Employee address cannot be blank")
+	@Size(min=3, max=50)
 	private String employeeAddress;
+	
+	@NotNull
+	@DecimalMin("10000")
+	@DecimalMax("99999")
 	private double employeeSalary;
 
 	public int getId() {
